797a169ac000c160c7a7c8706afcaf758aeedf1f
Garen <garengh4@gmail.com>
2022-10-11 15:09:30 -0400

    mat-material now available in shared. note flex is being used in AppComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,19 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material.module';
+import { LoginComponent } from './login/login.component';
+import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    MaterialModule,
-    FlexLayoutModule
+    SharedModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,11 +7,18 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
 import { MatButtonModule } from '@angular/material/button'
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
-
-// centralize Angular Material into this file
-
-const modules = [
+const sharedModules: any[] = [
+  FormsModule,
+  CommonModule,
+  BrowserModule,
+  BrowserAnimationsModule,
+  FlexLayoutModule,
   MatSidenavModule,
   MatToolbarModule,
   MatIconModule,
@@ -23,9 +30,8 @@ const modules = [
 ]
 
 @NgModule({
-  imports: modules,
-  exports: modules
+  imports: sharedModules,
+  exports: sharedModules
 })
 
-
-export class MaterialModule { }
+export class SharedModule { }
