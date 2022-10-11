a683eb8af70efc4d92ebc874bbaba115afa400b5
Garen <garengh4@gmail.com>
2022-10-11 15:11:59 -0400

    mat-material now available in shared. note flex is being used in AppComponent.

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from "@angular/core";
 import { SharedModule } from "../shared/shared.module";
-import { LoginComponent } from "./login.component";
-
 
 @NgModule({
-  declarations:[LoginComponent],
   imports:[
     SharedModule
   ]
